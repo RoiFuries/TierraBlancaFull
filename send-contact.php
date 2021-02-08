54b54ec37a0c81bb997099040f347835a096e0dc
RoiFuries <roi.furies@gmail.com>
2021-02-07 22:02:29 -0600

    changes on send-contat.php

--- send-contact.php
+++ send-contact.php
@@ -1,8 +1,8 @@
 <?php
 if($_POST)
 {
-	$to_Email   	= "admin@themes.ads"; //Replace with recipient email address
-	$subject        = 'Bistro - New Contact Inquiry'; //Subject line for emails
+	$to_Email   	= "royis.com@gmail.com"; //Replace with recipient email address
+	$subject        = 'TB - New Contact Inquiry'; //Subject line for emails
 	
 	
 	//check if its an ajax request, exit if not
